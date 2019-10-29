733bdb9807eb4f363c951dab97d9b3dbce1e2de3
Dan Langille <dan@langille.org>
2019-10-29 19:22:17 -0400

    Add our first stored procedures

---
+++ procedures.sql
@@ -0,0 +1,34 @@
+-- what packages are installed on this host
+-- SELECT * FROM PackagesOnHost('foo.example.org');
+
+CREATE OR REPLACE FUNCTION PackagesOnHost(text) returns SETOF text AS $$
+  SELECT P.name || '-' || PV.version
+    FROM host H JOIN host_package    HP ON H.name                = $1
+                                       AND H.id                  = HP.host_id
+                JOIN package_version PV ON HP.package_version_id = PV.id
+                JOIN package         P  ON PV.package_id         = P.id
+$$ LANGUAGE SQL STABLE;
+
+-- which hosts have this package in this version installed
+-- select * from HostsWithPackage('apr', '1.6.5.1.6.1_1');
+
+CREATE OR REPLACE FUNCTION HostsWithPackage(text,text) returns SETOF text AS $$
+SELECT H.name
+  FROM package P JOIN package_version PV ON P.name     = $1
+                                        AND P.id       = PV.package_id
+                                        AND PV.version = $2
+                 JOIN host_package HP    ON PV.id      = HP.package_version_id
+                 JOIN host         H     ON HP.host_id = H.id
+$$ LANGUAGE SQL STABLE;
+
+
+-- which hosts have this package. do not include version
+-- select * from HostsWithPackage('apr');
+
+CREATE OR REPLACE FUNCTION HostsWithPackage(text) returns SETOF text AS $$
+SELECT H.name
+  FROM package P JOIN package_version PV ON P.name     = $1
+                                        AND P.id       = PV.package_id
+                 JOIN host_package HP    ON PV.id      = HP.package_version_id
+                 JOIN host         H     ON HP.host_id = H.id
+$$ LANGUAGE SQL STABLE;

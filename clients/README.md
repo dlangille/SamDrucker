407713df5d4ee88da78cfaefc2860ce15f009844
Dan Langille <dan@langille.org>
2019-10-31 14:05:26 -0400

    Add the first client

---
+++ clients/README.md
@@ -0,0 +1,17 @@
+This directory contains SamDrucker clients in various languages.
+
+A client needs to:
+
+1. construct the JSON data
+1. urlencode it
+1. perform an http GET to the SamDrucker server
+
+You can use whatever tools you want to create a client.
+
+The scripts and dependencies are listed below:
+
+`samdrucker-py.sh`
+
+# https://github.com/jpmens/jo - for constructing JSON from a shell
+# python - for urlencoding
+
---
+++ clients/samdrucker-py.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+# we want to construct JSON which looks like this:
+# {
+#   "name": "test.example.org",
+#   "os": "FreeBSD",
+#   "version": "11.3-RELEASE-p4",
+#   "repo": "http://pkg.freebsd.org/FreeBSD:113:amd64/latest/",
+#   "packages": [
+#       "apr-1.6.5.1.6.1_1",
+#       "bacula9-client-9.4.3",
+#       "bash-5.0.7",
+#       "rsync-3.1.3_1",
+#       "serf-1.3.9_3",
+#       "sqlite3-3.29.0_1"
+#    ]
+# }
+#
+# I am using the FreeBSD textproc/jo:
+#
+# $ jo -p name=`hostname` os=`uname` version=`uname -r` repo=`pkg -vv | grep url | cut -f2 -d \"`
+# {
+#    "name": "samdrucker.int.unixathome.org",
+#    "os": "FreeBSD",
+#    "version": "12.0-RELEASE-p10",
+#    "repo": "pkg+http://fedex.unixathome.org/packages/120amd64-default-master-list/"
+# }
+#
+
+if [ -r /usr/local/etc/samdrucker/samdrucker.conf ]; then
+  . /usr/local/etc/samdrucker/samdrucker.conf
+fi
+
+# get list of packages on this host:
+
+pkg_args=""
+PKGS=`/usr/sbin/pkg info -q`
+for pkg in $PKGS
+do
+  pkg_args="$pkg_args packages[]=$pkg"
+done
+
+#echo $pkg_args
+
+hostname=`hostname`
+uname=`uname`
+version=`uname -r`
+repo=`pkg -vv | grep url | cut -f2 -d \"`
+
+payload=`/usr/local/bin/jo -p name=$hostname os=$uname version=$version repo=$repo $pkg_args`
+
+#echo "$payload"
+
+# I tried but failed with this attempt
+#encoded=`echo "$payload" | php -R 'echo rawurlencode(fgets(STDIN));'`
+
+# So I went with python
+encoded=`/usr/local/bin/python -c "import urllib; print urllib.quote('''$payload''')"`
+
+#echo "$encoded"
+
+curl --silent --output /dev/null $SAMDRUCKER_URL$SAMDRUCKER_ARG=$encoded

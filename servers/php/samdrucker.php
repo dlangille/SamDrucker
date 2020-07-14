a5436326c360859fcb772d151f677d3db13e1e49
dlangille <dan@langille.org>
2020-07-14 14:24:57 -0400

    Add server side configuration file This permits the user to keep this information outside the script allowing for better upgrades.

--- servers/php/samdrucker.php
+++ servers/php/samdrucker.php
@@ -1,17 +1,22 @@
 <?php
 
+define('CONF_FILE', '/usr/local/etc/samdrucker.conf');
+
+if (file_exists(CONF_FILE)) {
+  require_once(CONF_FILE);
+} else {
+  syslog(LOG_ERR, CONF_FILE . ' does not exist');
+  die(CONF_FILE . ' does not exist');
+  exit;
+}
+
 json_decode($_REQUEST['packages']);
 if (json_last_error() == JSON_ERROR_SYNTAX) {
+  # does this need some sanitizing?
   syslog(LOG_ERR, 'invalid json: ' . $_REQUEST['packages']);
   echo 'invalid JSON';
   exit;
 }
-
-$host     = 'pg02.int.unixathome.org';
-$port     = '5432';
-$db       = 'samdrucker';
-$user     = 'postie';
-$password = '[redacted]';
 
 $dsn = "pgsql:host=$host;port=$port;dbname=$db;user=$user;password=$password;sslmode=require";
 
@@ -22,6 +27,7 @@ try {
   $sql = 'SELECT HostAddPackages(:packages, :client_ip)';
 
   $stmt = $dbh->prepare($sql);
+  # does this need some sanitizing?
   $stmt->bindValue(':packages',  $_REQUEST['packages']);
   $stmt->bindValue(':client_ip', $_SERVER['REMOTE_ADDR']);
 
